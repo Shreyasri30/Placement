f467956f81e93b74f5a26990b80e1842094d50b9
Shreyasri30 <ramkishreya@gmail.com>
2025-08-25 14:53:00 +0530

    Add files via upload

--- 25-08-2025/cascadenew.sql
+++ 25-08-2025/cascadenew.sql
@@ -29,3 +29,40 @@ select * from dishes;
 update cuisine set cuisineid = 20 where cuisineid = 2;
 select * from cuisine;
 select * from dishes;
+
+select cuisineid AS Cuisine_No, cuisinename AS Cuisine_Name 
+from cuisine;
+
+select dishid AS Dish_No, dishname AS Food, cuisineid AS Cuisine_No
+from dishes;
+
+select dishid AS Dish_No, dishname AS Food
+from dishes
+where dishname LIKE '%pa%';
+
+select cuisineid AS Cuisine_No, cuisinename AS Cuisine_Name
+from cuisine
+where cuisinename LIKE '%ian';
+
+CREATE VIEW dish_view AS
+select 
+  d.dishid      AS Dish_No,
+  d.dishname    AS Food,
+  d.cuisineid   AS Cuisine_No,
+  c.cuisinename AS Cuisine_Name
+from dishes d, cuisine c
+where d.cuisineid = c.cuisineid;
+
+select * from dish_view;
+
+update dishes set dishname = 'Veg Pasta' where dishid = 105;
+select * from dish_view;
+
+delete from cuisine where cuisineid = 1;  
+select * from dish_view;
+
+update cuisine set cuisineid = 20 where cuisineid = 2;
+select * from dish_view;
+
+select * from cuisine;
+select * from dishes;
